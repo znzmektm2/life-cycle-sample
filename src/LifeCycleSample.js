6ae8f368248943a29263e648a8e7a542f17261c7
znzmektm2 <tingcber2@naver.com>
2020-05-22 21:47:36 +0900

    add: 에러 잡아내기(componentDidCatch)

--- src/LifeCycleSample.js
+++ src/LifeCycleSample.js
@@ -1,77 +1,76 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class LifeCycleSample extends Component {
   state = {
     number: 0,
-    color: null
-  }
+    color: null,
+  };
 
   myRef = null; // ref 설정할 부분
 
-  constructor (props) {
+  constructor(props) {
     super(props);
-    console.log('constructor');
+    console.log("constructor");
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    console.log('getDerivedStateFromProps', nextProps, prevState);
-    if(nextProps.color !== prevState.color) {
-      return {color: nextProps.color};
+    console.log("getDerivedStateFromProps", nextProps, prevState);
+    if (nextProps.color !== prevState.color) {
+      return { color: nextProps.color };
     }
     return null;
   }
 
   componentDidMount() {
-    console.log('componentDidMount');
+    console.log("componentDidMount");
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate', nextProps, nextState);
+    console.log("shouldComponentUpdate", nextProps, nextState);
     // 숫자의 마지막 자리가 4면 리렌더링 하지 않습니다.
     return nextState.number % 10 !== 4;
   }
 
   componentWillUnmount() {
-    console.log('componentWillUnmount');
+    console.log("componentWillUnmount");
   }
 
   handleClick = () => {
     this.setState({
-      number: this.state.number + 1
+      number: this.state.number + 1,
     });
-  }
+  };
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('getSnapshotBeforUpdate', prevProps, prevState);
-    if(prevProps.color !== this.props.color) {
+    console.log("getSnapshotBeforUpdate", prevProps, prevState);
+    if (prevProps.color !== this.props.color) {
       return this.myRef.style.color;
     }
     return null;
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('componentDidUpdate', this.props.color, prevProps.color);
-    if(snapshot) {
-      console.log('업데이트 되기 직전 색상: ', snapshot);
+    console.log("componentDidUpdate", this.props.color, prevProps.color);
+    if (snapshot) {
+      console.log("업데이트 되기 직전 색상: ", snapshot);
     }
   }
 
   render() {
-    console.log('render');
+    console.log("render");
 
     const style = {
-      color: this.props.color
+      color: this.props.color,
     };
 
     return (
       <div>
-        <h1 style={style} ref={ref => this.myRef = ref}>
-          {this.state.number}  
-        </h1>    
+        {this.props.missing.value}
+        <h1 style={style} ref={(ref) => (this.myRef = ref)}>
+          {this.state.number}
+        </h1>
         <p>color: {this.state.color}</p>
-        <button onClick={this.handleClick}>
-          더하기
-        </button>
+        <button onClick={this.handleClick}>더하기</button>
       </div>
     );
   }
